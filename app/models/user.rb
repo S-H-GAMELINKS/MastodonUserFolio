0b5c473592edac1506f3b709d2b058dc9a4f3974
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-08-15 23:07:18 +0900

    using display_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,13 @@ class User < ApplicationRecord
   def self.find_for_oauth(auth)
     user = User.where(uid: auth.uid, provider: auth.provider).first
       
+    if user != nil then
+      user.update!(uid: auth.extra.raw_info.display_name)
+    end
+
     unless user
       user = User.create(
-      uid:      auth.uid,
+      uid:      auth.extra.raw_info.display_name,
       provider: auth.provider,
       email:    User.dummy_email(auth),
       password: Devise.friendly_token[0, 20],
